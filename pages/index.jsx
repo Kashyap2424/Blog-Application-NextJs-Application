c003f3a75797adffa0292da3b776b075a6fe3a52
Kashyap-Pinak <kashyap@technolabs.com>
2022-12-16 22:01:37 +0530

    Displaying proper post on home page.

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,7 +14,7 @@ const Home = ({ posts }) => {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
-            <PostCard post={post} />
+            <PostCard key={index} post={post.node} />
           ))}
         </div>
 
--- services/index.js
+++ services/index.js
@@ -26,6 +26,7 @@ export const getPosts = async () => {
               name
               slug
             }
+            title
           }
         }
       }
