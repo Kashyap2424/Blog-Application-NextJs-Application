85dbac51a595ffabd1d44bb1ec5d3d2cb93ff045
Kashyap-Pinak <kashyap@technolabs.com>
2022-12-19 07:40:36 +0530

    Create categories widget.

---
+++ pages/posts/[slug].jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { getPosts, getPost } from "../../services";
+import {
+  PostDetails,
+  Categories,
+  PostWidget,
+  Author,
+  Comments,
+  CommentsForm,
+} from "../../Components";
+
+const PostDetails = () => {
+  return <div>PostDetails</div>;
+};
+
+export default PostDetails;
